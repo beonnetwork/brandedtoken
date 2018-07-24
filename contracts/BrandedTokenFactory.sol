13d47fe8f734109edbc8bb51a5db4300714ac5c1
Surat Teerapittayanon <steerapi@gmail.com>
2018-07-24 21:54:00 +0700

    Add transfer ownership from factory

--- contracts/BrandedTokenFactory.sol
+++ contracts/BrandedTokenFactory.sol
@@ -1,18 +1,25 @@
 pragma solidity ^0.4.24;
 import "./Factory.sol";
 import "./BrandedToken.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 
 contract BrandedTokenFactory is Factory {
+    using SafeMath for uint256;
+    using SafeERC20 for BrandedToken;
 
     function create(uint256 _initialAmount,
         string _tokenName,
         uint8 _decimalUnits,
         string _tokenSymbol)
         public
-        returns (address brandedToken)
+        returns (address brandedTokenAddress)
     {
-        brandedToken = new BrandedToken(_initialAmount, _tokenName, _decimalUnits, _tokenSymbol);
-        register(brandedToken);
+        BrandedToken brandedToken = new BrandedToken(_initialAmount, _tokenName, _decimalUnits, _tokenSymbol);
+        brandedToken.safeTransfer(msg.sender, _initialAmount);
+        brandedToken.transferOwnership(msg.sender);
+        brandedTokenAddress = address(brandedToken);
+        register(brandedTokenAddress);
     }
 
 }
